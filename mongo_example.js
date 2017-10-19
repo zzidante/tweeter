1251a9c584e45f3748360a25141b50d1c5a043f8
Mel <zzidante@gmail.com>
2017-10-19 23:25:48 +0000

    connection example complete

--- mongo_example.js
+++ mongo_example.js
@@ -1,15 +1,26 @@
 "use strict";
 
-const MongoClient = require("mongodb").MongoClient;
+const {MongoClient} = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
 
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // Mongo 'Find' all the tweets chaining find and convertion.
-  db.collection("tweets").find().toArray((err, results) => {
-    console.log("results array: ", results);
-  })
-  db.close();
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray(callback);
+  }
+                      // It can be invoked later -  
+                      // when passed to another scope, it still has 
+                      // closure over db so it'll work
+
+  getTweets((err, tweets) => {
+    console.log("Logging each tweet: "); // <<<
+    for (let tweet of tweets) {
+      console.log("Hey" + tweet); // <<<
+    };
+    db.close();
+  });
+  // remember to put the closing connection to database after the logic function.
+
 });
