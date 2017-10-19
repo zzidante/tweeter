c22ba644253743ea8a6be20fa69459f892129ac6
Mel <zzidante@gmail.com>
2017-10-19 23:04:18 +0000

    Get filtered list of objects using callback in a callback

--- mongo_example.js
+++ mongo_example.js
@@ -4,21 +4,15 @@ const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    console.log("Failed to connect");
-    throw err;
-  }
+
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
   // Mongo 'Find' all the tweets.
-  db.collection("tweets").find({}, (err, result) => {
+  db.collection("tweets").find({}, (err, results) => {
 
-    // so much data
-    console.log("find result ", result);
-    console.log("type of find result: ", typeof result);
+      // we can iterate on the CURSOR to get results, one at a time
+    console.log("for each item YIELDED by cursor");
+    results.each((err, item) => console.log(" ", item));
   })
   db.close();
 });
-
-// a cursor is an object that will give you one item at a time (if you want).
-    // more memory efficient but less convienient than an array
