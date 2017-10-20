8565085fe1927daa9f9572c1700f9a03444447c1
Mel <zzidante@gmail.com>
2017-10-20 21:00:35 +0000

    Adjusted character counter and added word wrap-break for when post has no spaces.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,7 +18,7 @@ $(function() {
   
   var tweetForm = $('#new-tweet-form');
   var newTweetTextArea = $('.new-tweet textarea');
-  var newTweetErrorText = $('span.new-tweet-warning');   // why can't I use that to remove 0 length error warning?
+  var newTweetErrorText = $('span.new-tweet-warning');
 
 
   // New Tweet Form Submission Helper Functions
@@ -31,14 +31,9 @@ function addZeroTextWarning() {
 function removeZeroTextWarning() {
   newTweetTextArea.focus(function(event) {
     $('span.new-tweet-warning').remove();
-    $('.counter').html(140);    // remove leftover red on reset
+    $('.counter').html(140);
   });
 }
-
-function addZeroTextWarning() {
-    tweetForm
-    .append('<span class="new-tweet-warning">You must have forgotten to insert some text! Try again!</span>')
-};
 
   function loadTweets () {
     $.ajax({
@@ -50,27 +45,15 @@ function addZeroTextWarning() {
     });
   }
 
-
-  // Compose Button Click Toggle Logic 
-
-  $("#compose-tweet-button").on("click", function () {
-    $("section.new-tweet").slideToggle(400);
-  })
-
-
-  tweetForm.submit(function(event) {
-    event.preventDefault();
-    var newTweetForm = this;
-    var newTweetSerialize = $(this).serialize();
-    var tweetTextArea = $('.tweet-user-input').val();  
-
+  function checkIfTweetsValidPostIfYes(newTweetForm, newTweetSerialize, tweetTextArea) {
     if (tweetTextArea.length && tweetTextArea !== " ") {
       if (tweetTextArea.length > 140) {
         tweetForm 
-          .append('<span class="new-tweet-warning">Characters cannot exceed 140 characters. Try again!</span>');        
+          .append('<span class="new-tweet-warning">Characters cannot exceed 140 characters. Try again!</span>');  
           newTweetTextArea.focus(function(event) {
             newTweetErrorText.remove();
-          newTweetTextArea.val('');
+            $('.counter').html(140);
+            newTweetTextArea.val('');
         });
       } else {
         $.ajax({
@@ -80,15 +63,34 @@ function addZeroTextWarning() {
         }).done(function() {     
           newTweetForm.reset();
           loadTweets();
-        }); 
+        });
       }
     } else {
       addZeroTextWarning();
-      removeZeroTextWarning();  // add condition for empty space.
+      removeZeroTextWarning();
     }
+  }
+
+  // Compose Button Click Toggle Logic 
+
+  $("#compose-tweet-button").on("click", function () {
+    $("section.new-tweet").slideToggle(400);
+  })
+
+
+  // Post New Tweet
+
+  tweetForm.submit(function(event) {
+    event.preventDefault();
+    var newTweetForm = this;
+    var newTweetSerialize = $(this).serialize();
+    var tweetTextArea = $('.tweet-user-input').val().trim();
+
+    checkIfTweetsValidPostIfYes(newTweetForm, newTweetSerialize, tweetTextArea);
   });
+
+
+  // Get tweets
+
     loadTweets();
 });
-
-
-// compose-tweet-button 
