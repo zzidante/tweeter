3916335791a28c74f6a6a8b5d046a916b1077a9c
Mel <zzidante@gmail.com>
2017-10-27 06:04:39 +0000

    changed button colors, fixed 0 being red, re-aligned time-ago on tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,7 @@ $(function() {
     var tweetCharacterCount = maxNumCharacters - counterLength;
     var counterDisplayNumber = $(this).parent().find('.counter').html(tweetCharacterCount);
 
-    if (tweetCharacterCount <= 0) {
+    if (tweetCharacterCount < 0) {
       counterDisplayNumber.css('color', 'red');
     } else {
       counterDisplayNumber.css('color', "");  // reset back to original color
